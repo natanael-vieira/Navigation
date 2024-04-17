450fe372573da9fb75337599039d37c7645f909e
Natanael Vieira <89619697+natanael-vieira@users.noreply.github.com>
2024-04-17 08:00:07 -0300

    Update TelaA.js

--- src/views/TelaA.js
+++ src/views/TelaA.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import TextoCentral from '../components/TextoCentral'
+import PassoStack from '../components/PassoStack'
 
 export default props => {
     return (
-        <TextoCentral corFundo='#e53935'>
-            Tela A
-        </TextoCentral>
+        <PassoStack {...props} avancar='TelaB' >
+            <TextoCentral corFundo='#e53935'>
+                Tela A
+            </TextoCentral>
+        </PassoStack>
 )}
