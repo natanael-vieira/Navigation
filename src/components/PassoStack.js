1c29168bacc636e2edf066c749d8d34be344fbda
Natanael Vieira <89619697+natanael-vieira@users.noreply.github.com>
2024-04-18 06:41:58 -0300

    Update PassoStack.js

--- src/components/PassoStack.js
+++ src/components/PassoStack.js
@@ -3,12 +3,21 @@ import { View, Text, Button } from 'react-native'
 
 export default props => (
     <View style={{ flex: 1 }}> 
-        <View>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
             {props.avancar
                 ? <Button 
                     title='Próxima'
                     onPress={() => {
                         props.navigation.navigate(props.avancar)
+                    }}
+                    />
+                : false
+            }
+            {props.voltar
+                ? <Button 
+                    title='Anterior'
+                    onPress={() => {
+                        props.navigation.goBack()
                     }}
                     />
                 : false
