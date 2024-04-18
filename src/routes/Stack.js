5d050629b71dc8577e6ba465fd0fae344c5692f3
Natanael Vieira <89619697+natanael-vieira@users.noreply.github.com>
2024-04-18 06:34:00 -0300

    Update Stack.js

--- src/routes/Stack.js
+++ src/routes/Stack.js
@@ -6,6 +6,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import TelaA from '../views/TelaA'
 import TelaB from '../views/TelaB'
 import TelaC from '../views/TelaC'
+import PassoStack from '../components/PassoStack'
 
 const Stack = createNativeStackNavigator()
 
@@ -13,9 +14,21 @@ export default props => (
     <Stack.Navigator initialRouteName='TelaA' 
         screenOptions={{ headerShown: true }}>
         <Stack.Screen name='TelaA' 
-            options={{ title: 'Informações Iniciais' }}
-            component={TelaA} />
-        <Stack.Screen name='TelaB' component={TelaB} />
+            options={{ title: 'Informações Iniciais' }}>
+                {props => (
+                    <PassoStack {...props} avancar='TelaB'>
+                        <TelaA />
+                    </PassoStack>
+                )}
+            </Stack.Screen>
+        <Stack.Screen name='TelaB' 
+            options={{ title: 'Tela B' }}>
+                {props => (
+                    <PassoStack {...props} avancar='TelaC'>
+                        <TelaB />
+                    </PassoStack>
+                )}
+            </Stack.Screen>
         <Stack.Screen name='TelaC' component={TelaC} />
     </Stack.Navigator>
 )
