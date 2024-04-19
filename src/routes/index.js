c666e18f51cfb7132d1572033b0a2e99336706b9
Natanael Vieira <89619697+natanael-vieira@users.noreply.github.com>
2024-04-19 07:54:00 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,19 @@
+import 'react-native-gesture-handler'
 import React from 'react'
 import { SafeAreaView } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 
-import Stack from './Stack'
-import Tab from './Tab'
+//import Stack from './Stack'
+//import Tab from './Tab'
+import Drawer from './Drawer'
 
 export default props => (
     <SafeAreaView style={{
         flex: 1
     }}>
         <NavigationContainer>
-            <Tab />
+            <Drawer />
+            {/* {<Tab />} */}
             {/* {<Stack />} */}
         </NavigationContainer>
     </SafeAreaView>
