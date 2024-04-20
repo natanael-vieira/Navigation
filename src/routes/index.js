253931219bed065d064c66bd1629841f2389c1bc
Natanael Vieira <89619697+natanael-vieira@users.noreply.github.com>
2024-04-20 06:21:15 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,16 +4,16 @@ import { SafeAreaView } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 
 //import Stack from './Stack'
-//import Tab from './Tab'
-import Drawer from './Drawer'
+import Tab from './Tab'
+// import Drawer from './Drawer'
 
 export default props => (
     <SafeAreaView style={{
         flex: 1
     }}>
         <NavigationContainer>
-            <Drawer />
-            {/* {<Tab />} */}
+            {/* {<Drawer />} */}
+            <Tab />
             {/* {<Stack />} */}
         </NavigationContainer>
     </SafeAreaView>
