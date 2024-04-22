698d4bff9aa0666a2671318e4e8da04d2cbfc830
Natanael Vieira <89619697+natanael-vieira@users.noreply.github.com>
2024-04-22 06:35:33 -0300

    Update Tab.js

--- src/routes/Tab.js
+++ src/routes/Tab.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs' 
+import Ionicons from 'react-native-vector-icons/Ionicons'
 import TelaA from '../views/TelaA'
 import TelaB from '../views/TelaB'
 import TelaC from '../views/TelaC'
@@ -7,13 +8,40 @@ import TelaC from '../views/TelaC'
 const Tab = createBottomTabNavigator()
 
 export default props => (
-    <Tab.Navigator screenOptions={{
-        tabBarActiveTintColor: 'red',
-        tabBarInactiveTintColor: 'blue',
-        tabBarLabelStyle: { fontSize: 30 }
-    }} initialRouteName='TelaC'>
-        <Tab.Screen name='TelaA' component={TelaA} />
-        <Tab.Screen name='TelaB' component={TelaB} />
-        <Tab.Screen name='TelaC' component={TelaC} />
+    <Tab.Navigator 
+        screenOptions= {({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+
+                switch (route.name) {
+                    case 'TelaA':
+                        iconName = focused
+                            ? 'ios-information-circle'
+                            : 'ios-information-circle-outline'
+                        break
+                    
+                    case 'TelaB':
+                        iconName = focused
+                            ? 'ios-information-circle'
+                            : 'ios-information-circle-outline'
+                        break
+                    
+                    case 'TelaC':
+                        iconName = focused ? 'ios-list' : 'ios-list-outline'
+                        break
+                }
+                return <Ionicons name={iconName} size={size} color={color} />
+            },
+            tabBarActiveTintColo: 'red',
+            tabBarInactiveTintColor: 'blue',
+            tabBarShowLabel: true,
+            // tabBarLabelStyle: { fontSize: 30 }
+
+
+        })
+    } initialRouteName='TelaC'>
+        <Tab.Screen name='TelaA' component={TelaA} options={{ title: 'Inicial' }} />
+        <Tab.Screen name='TelaB' component={TelaB} options={{ title: 'Meio' }} />
+        <Tab.Screen name='TelaC' component={TelaC} options={{ title: 'Final' }} />
     </Tab.Navigator>
 )
